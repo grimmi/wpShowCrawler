92c60dd20091c9728f0b3e079f1010df6cb7ca98
Arne Lehmann <arne.lehmann@gmx.de>
2016-09-23 21:03:47 +0200

    Projektdateien hinzufügen.

---
+++ WikipediaShowCrawler/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikipediaShowCrawler
+{
+    public class Class1
+    {
+    }
+}
