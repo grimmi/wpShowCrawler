7b579d429c9a81674053d7c78ea9b2e0d236dc67
Arne Lehmann <arne.lehmann@gmx.de>
2016-09-23 21:11:18 +0200

    test projekt hinzugefügt

---
+++ WikipediaShowCrawler.Tests/EpisodeListDownloaderTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WikipediaShowCrawler.Tests
+{
+    [TestFixture]
+    public class EpisodeListDownloaderTests
+    {
+        [Test]
+        public async Task DownloadPage_ExistingWikipediaPageTitle_SuccessfullyDownloadPage()
+        {
+            var epListDownloader = new EpisodeListDownloader("Modern Family");
+
+            var episodeList = await epListDownloader.DownloadEpisodeList();
+
+            Assert.AreEqual("Modern Family", episodeList.ShowName);
+        }
+    }
+}
